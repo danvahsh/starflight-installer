53fe3602e13346fe70f263a917614fe7118a7f7c
Daniel Vahsholtz <106500790+danvahsh@users.noreply.github.com>
2023-02-18 22:26:56 -0700

    Changes

--- updater/main.py
+++ updater/main.py
@@ -1,27 +1,30 @@
 import requests
 import json
+import sys
 
-def read_index():
+def request_index():
     online_index = requests.get('https://raw.githubusercontent.com/danvahsh/starflight-installer-meta/main/index.json')
 
     return_info = dict(status_code=online_index.status_code, text=online_index.text)
 
     return return_info
 
+raw_index = request_index()
+
 def check_index(json_text):
-    index_info = json.loads(read_index()['text'])
+    index_info = json.loads(raw_index['text'])
 
-    if index_info['index_type'] == "SFUPDATERINDEX" and index_info['version'] == 1.0
+    if index_info['index_type'] == "SFUPDATERINDEX" and index_info['index_style_version'] == 1.0:
+        pass
 
-def read_json(json_text):
-    main_index = json.loads(read_index()['text'])
-    versions = json.loads(main_index['versions'])
+def load_json(json_text):
+    index = json.loads(raw_index()['text'])
 
 def main_updater():
-    json_index = read_index()
+    json_index = index()
 
-    if json_index['staus_code'] != 200
-        break
+    if json_index['staus_code'] != 200:
+        pass
 
 
 print(input('press enter to exit'))
